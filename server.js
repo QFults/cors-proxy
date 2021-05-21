df92204ccd6a324679f89787f371783811415b0d
Quinton Fults <quinton.fults.work@gmail.com>
2021-05-21 09:35:06 -0700

    code

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.use(require('cors')())
+
+app.get('*', (req, res) => {
+  const targetURL = req.header('Target-URL')
+  if (!targetURL) {
+    res.status(500).send({ error: 'There is no Target-URL header in the request' })
+  }
+  axios.get(targetURL)
+    .then(response => {
+      console.log(response)
+      res.json(response.data)
+    })
+    .catch(err => res.status(500).send(err))
+})
+
+app.listen(process.env.PORT || 3000)
